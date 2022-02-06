26cdda793d560b6281eaad22124fdab019e3dc94
Seiya Terada <54200250+seiyaterada@users.noreply.github.com>
2022-02-06 03:52:54 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xC63d9E46FA55DB3c44118379C801dBcbAC31a09B';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
